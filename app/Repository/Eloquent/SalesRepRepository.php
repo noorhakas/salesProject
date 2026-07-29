dd4b327996b19c958dfb5b3c548508320f8ab139
noorhakas <noura.hassan@kasme.com>
2026-07-29 22:17:52 +0300

    sales profile

--- app/Repository/Eloquent/SalesRepRepository.php
+++ app/Repository/Eloquent/SalesRepRepository.php
@@ -58,9 +58,6 @@ class SalesRepRepository implements SalesRepInterface
 
         $subordinateIds = $manager->getAllSubordinateIds();
 
-        print_r($subordinateIds);
-        print_r($salesRep->id);
-        dd('');
         if (! in_array($salesRep->id, $subordinateIds)) {
             return [
                 'status' => false,
