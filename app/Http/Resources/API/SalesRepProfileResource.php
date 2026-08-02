2526f6da81cd343f380dd34e830b05aed6c5bcd4
noorhakas <noura.hassan@kasme.com>
2026-08-02 10:51:32 +0300

    visits

--- app/Http/Resources/API/SalesRepProfileResource.php
+++ app/Http/Resources/API/SalesRepProfileResource.php
@@ -54,12 +54,12 @@ class SalesRepProfileResource extends JsonResource
 		return $base;
     }
 
-	public static function collection($resource)
-    {
-        return tap(new GlobalCollection($resource, static::class), function ($collection) {
-            if (property_exists(static::class, 'preserveKeys')) {
-                $collection->preserveKeys = (new static([]))->preserveKeys === true;
-            }
-        });
-   }
+// 	public static function collection($resource)
+//     {
+//         return tap(new GlobalCollection($resource, static::class), function ($collection) {
+//             if (property_exists(static::class, 'preserveKeys')) {
+//                 $collection->preserveKeys = (new static([]))->preserveKeys === true;
+//             }
+//         });
+//    }
 }
