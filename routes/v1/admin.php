159a60ff37c8c093b194bdd7695a0263b760eb0c
noorhakas <noura.hassan@kasme.com>
2026-08-05 01:03:13 +0300

    unauthorized

--- routes/v1/admin.php
+++ routes/v1/admin.php
@@ -28,7 +28,7 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::resource('roles', 'RoleController')->except(['edit', 'create']);;
     Route::get('permissions', 'RoleController@allPermissions');
 	Route::apiResource('managers', 'ManagerController');
-    Route::get('manager_profile/{user}', 'ManagerController@profile');
+    Route::get('manager_profile/{manager}', 'ManagerController@profile');
 
    
    ###setting
