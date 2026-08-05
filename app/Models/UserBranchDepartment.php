dfe9c4c1ec1cff39e960e809f57b74e3ac861597
noorhakas <noura.hassan@kasme.com>
2026-08-06 02:13:10 +0300

    Add attendance

--- app/Models/UserBranchDepartment.php
+++ app/Models/UserBranchDepartment.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserBranchDepartment extends Model
 {
     protected $fillable = ['user_id', 'branch_id', 'department_id'];
+        protected $table = 'user_branch_departments';
 
     public function branch()
     {
