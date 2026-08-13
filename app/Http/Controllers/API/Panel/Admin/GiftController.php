3fbabf64c3a53ac675d84f2f1caa33c730cef088
noorhakas <noura.hassan@kasme.com>
2026-08-13 23:57:31 +0300

    bricks

--- app/Http/Controllers/API/Panel/Admin/GiftController.php
+++ app/Http/Controllers/API/Panel/Admin/GiftController.php
@@ -9,56 +9,45 @@ use App\Repository\Interfaces\GiftInterface;
 
 class GiftController extends Controller
 {
-	public $IGift;
+    public $IGift;
+
     public function __construct(GiftInterface $IGift)
     {
         $this->IGift = $IGift;
+
+        $this->middleware('permission:display Gift')->only(['index', 'show']);
+        $this->middleware('permission:create Gift')->only(['store']);
+        $this->middleware('permission:update Gift')->only(['update']);
+        $this->middleware('permission:delete Gift')->only(['destroy']);
     }
 
-	public function index(Request $request)
-	{
-		// if (!auth()->user()->hasPermissionTo('display Acc-Type'))
-		// 	return $this->SendResponse(["status"=>false, "message"=>__('messages.permission_denied')],403);
-
-		$response = $this->IGift->getAll($request);
-		return $this->SendResponse($response);
-	}
-
-	public function store(GiftRequest $request)
+    public function index(Request $request)
     {
-		//if (!auth()->user()->hasPermissionTo('create Acc-Type'))
-		//	return $this->SendResponse(["status"=>false, "message"=>__('messages.permission_denied')],403);
-
-		$response = $this->IGift->createGift($request);
-		return $this->SendResponse($response);     
+        $response = $this->IGift->getAll($request);
+        return $this->SendResponse($response);
     }
 
-	public function show($id)
+    public function store(GiftRequest $request)
     {
-		if (!auth()->user()->hasPermissionTo('display Acc-Type'))
-			return $this->SendResponse(["status"=>false, "message"=>__('messages.permission_denied')],403);
-
-		$response = $this->IGift->show($id);
-		return $this->SendResponse($response);
+        $response = $this->IGift->createGift($request);
+        return $this->SendResponse($response);
     }
 
-	public function update(GiftRequest $request,$id) {
-		//if (!auth()->user()->hasPermissionTo('update Acc-Type'))
-		//	return $this->SendResponse(["status"=>false, "message"=>__('messages.permission_denied')],403);
-
-		$response = $this->IGift->updateGift($request,$id);
-		return $this->SendResponse($response);
-      
-	}
-	public function destroy($id)
+    public function show($id)
     {
-		//if (!auth()->user()->hasPermissionTo('delete Acc-Type'))
-		//	return $this->SendResponse(["status"=>false, "message"=>__('messages.permission_denied')],403);
-
-		$response = $this->accType->deleteAccType($id);
-		return $this->SendResponse($response);
-	 
+        $response = $this->IGift->show($id);
+        return $this->SendResponse($response);
     }
 
+    public function update(GiftRequest $request, $id)
+    {
+        $response = $this->IGift->updateGift($request, $id);
+        return $this->SendResponse($response);
+    }
 
+    public function destroy($id)
+    {
+        $response = $this->IGift->deleteGift($id);
+        return $this->SendResponse($response);
+    }
 }
