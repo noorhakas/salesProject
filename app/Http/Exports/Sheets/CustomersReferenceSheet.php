b5314fce21c3ea75916dc90e7eff5e02b35aac5d
noorhakas <noura.hassan@kasme.com>
2026-08-19 02:21:11 +0300

    accounts&customers

--- app/Http/Exports/Sheets/CustomersReferenceSheet.php
+++ app/Http/Exports/Sheets/CustomersReferenceSheet.php
@@ -43,8 +43,10 @@ class CustomersReferenceSheet implements
             'Class',
             'Phone',
             'Phone 1',
-            'Brief'
-
+            'Brief',
+            'Work Days',
+            'Work Time From',
+            'Work Time To',
         ];
     }
 
@@ -52,15 +54,84 @@ class CustomersReferenceSheet implements
     {
         return [
             $customer->Uuid,
+
             optional($customer->account)->name,
+
             optional($customer->accType)->name,
+
             $customer->name,
+
             optional($customer->specialty)->name ?? '',
+
             optional($customer->class)->name ?? '',
+
             $customer->phone ?? '',
+
             $customer->phone_1 ?? '',
+
             $customer->brief ?? '',
+
+            $this->formatWorkDays($customer->work_days),
+
+            $customer->work_start_time ?? '',
+
+            $customer->work_end_time ?? '',
         ];
+    }
+
+    /**
+     * Convert work_days IDs to day names.
+     *
+     * Example:
+     * [1, 3, 7]
+     *
+     * becomes:
+     * SAT, MON, FRI
+     */
+    protected function formatWorkDays($workDays): string
+    {
+        if (empty($workDays)) {
+            return '';
+        }
+
+        if (is_string($workDays)) {
+            $decoded = json_decode($workDays, true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $workDays = $decoded;
+            } else {
+                $workDays = explode(',', $workDays);
+            }
+        }
+
+        if (!is_array($workDays)) {
+            return '';
+        }
+
+        $days = [
+            1 => 'SAT',
+            2 => 'SUN',
+            3 => 'MON',
+            4 => 'TUES',
+            5 => 'WEND',
+            6 => 'THUR',
+            7 => 'FRI',
+        ];
+
+        return collect($workDays)
+            ->map(function ($day) use ($days) {
+                /*
+                 * In case the value is:
+                 * { "id": 1 }
+                 */
+                if (is_array($day)) {
+                    $day = $day['id'] ?? null;
+                }
+
+                return $days[(int) $day] ?? null;
+            })
+            ->filter()
+            ->implode(', ');
     }
 
     public function title(): string
@@ -70,6 +141,19 @@ class CustomersReferenceSheet implements
 
     protected function columns(): array
     {
-        return ['A','B','C','D','E','F','G','I','J'];
+        return [
+            'A', // CODE
+            'B', // Account Name
+            'C', // Account Type
+            'D', // Customer Name
+            'E', // Specialty
+            'F', // Class
+            'G', // Phone
+            'H', // Phone 1
+            'I', // Brief
+            'J', // Work Days
+            'K', // Work Time From
+            'L', // Work Time To
+        ];
     }
 }
