7f267e1b76f3cf08a19995bc3320baea6a67c403
noorhakas <noura.hassan@kasme.com>
2026-09-14 13:46:22 +0300

    import customer

--- app/Http/Imports/Sheets/CustomersImportSheet copy.php
+++ app/Http/Imports/Sheets/CustomersImportSheet copy.php
@@ -10,10 +10,8 @@ use App\Models\Specialty;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithCalculatedFormulas; // <-- ضيف دي
 
-
-class CustomersImportSheet implements ToCollection, WithHeadingRow, WithCalculatedFormulas
+class CustomersImportSheet implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
